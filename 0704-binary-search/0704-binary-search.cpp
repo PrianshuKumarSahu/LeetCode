26669b9fe186c3b966d71ceded79cb7314a85efd
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-07-05 07:57:33 +0530

    Time: 0 ms (100%), Space: 31.5 MB (7.28%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,17 +4,16 @@ public:
         int n = nums.size();
         int st = 0;
         int end = n-1;
-        int ans = -1;
-        for (int i = 0; i<n; i++){
+        while (st<=end){
             int mid = (st+end)/2;
             if (target > nums[mid]){
                 st = mid+1;
             }else if (target < nums[mid]){
                 end = mid-1;
             }else{
-                ans = mid;
+                return mid;
             }
         }
-        return ans;
+        return -1;
     }
 };
