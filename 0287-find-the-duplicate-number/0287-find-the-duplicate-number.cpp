4e9f6daa79fa1508da0781fef0ef462cd7b1bb9f
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-07-25 17:15:53 +0530

    Time: 0 ms (100%), Space: 65 MB (91.03%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& arr) {
-        unordered_set<int> s;
+        int slow = arr[0], fast = arr[0];
 
-        for(int val : arr){
-            if(s.find(val) != s.end()) {
-                return val;
-            }
+        do{
+            slow = arr[slow];
+            fast = arr[arr[fast]];
+        }while (slow != fast);
 
-            s.insert(val);
+        slow = arr[0];
+
+        while(slow != fast){
+            slow = arr[slow];
+            fast = arr[fast];
         }
-
-        return -1;
+        return slow;
     }
 };
