1514307429db02ed44eec0b127f98df0dc604e18
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-08-03 19:56:36 +0530

    Time: 0 ms (100%), Space: 31.3 MB (82.43%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    /*int search(vector<int>& nums, int target) {
         int n = nums.size();
         int st = 0;
         int end = n-1;
@@ -16,5 +16,25 @@ public:
             }
         }
         return ans;
+    }*/
+
+    //helper function
+    int binSearch(vector<int>& arr, int tar, int st, int end){
+        if(st <= end){
+            int mid = st + (end-st)/2;
+
+            if(arr[mid] == tar) return mid;
+            else if(arr[mid] <= tar){
+                return binSearch(arr, tar, mid+1, end);
+            }else{
+                return binSearch(arr, tar, st, mid-1);
+            }
+
+        }
+        return -1;
+    }
+
+    int search(vector<int>& arr, int tar){
+        return binSearch(arr, tar, 0, arr.size()-1);
     }
 };
