395201e610419660daf29a23b348830c4c24d2a3
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-08-14 20:56:13 +0530

    Time: 0 ms (100%), Space: 263.5 MB (5.73%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
+
+    void reverseArray(vector<int>& nums, int start, int end) {
+        while (start < end) {
+            swap(nums[start], nums[end]);
+            start++;
+            end--;
+        }
+    }
+
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
+        if (n == 0 || k == 0) return;
         k = k%n;
-        vector<int> temp(k);
+
+        reverseArray(nums, 0, n - 1);
+        reverseArray(nums, 0, k - 1);
+        reverseArray(nums, k, n - 1);
+
+
+        //BETTER 
+        /*vector<int> temp(k);
         //int temp[k];
         for (int i = 0; i<k; i++){
             temp[i] = nums[n - k + i];
@@ -14,7 +31,9 @@ public:
 
         for (int i = 0; i < k; i++) {
             nums[i] = temp[i];
-        }
+        }*/
+
+
         // Brute force TLE
         /*int n = nums.size();
         vector<int> copy(n);
