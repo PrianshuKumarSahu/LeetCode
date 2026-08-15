64ec912a99bb79a94bd76b23d1d334f16f491f32
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-08-15 16:40:02 +0530

    Time: 0 ms (100%), Space: 28.2 MB (43.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,6 +3,7 @@ public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
 
+        /*
         for (int  i = 0; i<n; i++){         // Brute force approach
             int count=0;
             for(int j =0; j<n; j++){
@@ -13,10 +14,9 @@ public:
             if (count > n/2){
                 return nums[i];
             }
-        }
+        }*/
         
-        
-        /*
+    
         int freq=0, ans=0;              //Optimal approach
         for (int i=0; i<n; i++){           // Dutch national flag algo
             if (freq == 0){
@@ -28,7 +28,7 @@ public:
                 freq--;
             }
         }
-        return ans;*/
+        return ans;
 
         return 0;
     }
