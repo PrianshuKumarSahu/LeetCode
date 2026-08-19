419482b908a8518c4e0df8e0144d87e9c3db908a
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-08-19 22:16:00 +0530

    Time: 0 ms (100%), Space: 8.5 MB (64.99%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,11 +3,11 @@ public:
     int reverse(int x) {
         int revNum = 0;
         while (x!=0){
-            int digit = x%10;
+            int dig = x%10;
             if (revNum > INT_MAX/10 || revNum < INT_MIN/10){
                 return 0;
             }
-            revNum = revNum * 10 + digit;
+            revNum = revNum * 10 + dig;
             x = x/10; 
         }
         return revNum;
