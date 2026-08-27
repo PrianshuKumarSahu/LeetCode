0965478154f768ff48dbb7b7368a37f2498302ba
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-08-27 19:23:03 +0530

    Time: 5 ms (20%), Space: 8.6 MB (20.49%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,11 +3,11 @@ public:
     int reverse(int n) {
         int revNum = 0;
         while (n!=0){
-            int dig = n%10;
+            int digit = n%10;
             if (revNum > INT_MAX/10 || revNum < INT_MIN/10){
                 return 0;
             }
-            revNum = revNum * 10 + dig;
+            revNum = revNum * 10 + digit;
             n = n/10; 
         }
         return revNum;
