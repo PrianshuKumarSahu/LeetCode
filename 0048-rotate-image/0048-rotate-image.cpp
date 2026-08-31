11a16160637431f276712114aed0deb322305ebc
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-08-31 14:57:49 +0530

    Time: 0 ms (100%), Space: 10.1 MB (70.52%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,6 +2,21 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
+
+        //OPTIMAL
+        for(int i = 0; i<n-1; i++){
+            for (int j=i; j<n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i<n ; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+
+
+        //BRUTE FORCE APPROCH
+        /*
         vector<vector<int>> ans(n,vector<int>(n));
         for (int i = 0; i<n; i++){
             for (int j = 0; j<n; j++){
@@ -13,6 +28,7 @@ public:
                 matrix[i][j] = ans [i][j];
             }
         }
+        */
 
 
     }
