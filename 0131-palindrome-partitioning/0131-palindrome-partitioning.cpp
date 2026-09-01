2f221475e8c0d4d6de89b6df1669f0a948329d0d
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-09-01 20:55:01 +0530

    Time: 43 ms (58.43%), Space: 54.6 MB (60.96%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
 
     bool isPalin(string s){
-        string s2 = s;
-        reverse(s2.begin(), s2.end());
-        return s == s2;
+        string sCopy = s;
+        reverse(sCopy.begin(), sCopy.end());
+        return s == sCopy;
     }
 
     void getAllParts(string s, vector<string> &partitions, vector<vector<string>> &ans){
