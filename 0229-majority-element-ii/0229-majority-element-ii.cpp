4492bccd527246ff255c229cf755f9cc4687095e
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-09-05 17:20:43 +0530

    Time: 7 ms (40.19%), Space: 26.6 MB (18.81%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans;
+        vector<int> result;
+        
+        /*
         int x = n/3;
         for (int  i = 0; i<n; i++){         // Brute force approach
-            if(ans.size() == 0 || ans[0] != nums[i]){
+            if(result.size() == 0 || result[0] != nums[i]){
                 int count=0;
                 for(int j =0; j<n; j++){
                     if (nums[i] == nums[j]){
@@ -13,13 +15,30 @@ public:
                     }
                 }
                 if (count > n/3){
-                    ans.push_back(nums[i]);
+                    result.push_back(nums[i]);
                 }
             }
-            if (ans.size() == 2){
+            if (result.size() == 2){
                 break;
             }
         }
-        return ans;
+        */
+
+        unordered_map<int, int> mpp;
+        int minimum = int(n/3) + 1;
+
+        for (int  i = 0; i < n; i++){
+            mpp[nums[i]]++;
+
+            if (mpp[nums[i]] == minimum){
+                result.push_back(nums[i]);
+            }
+
+            if(result.size() == 2){
+                break;
+            }
+        }
+
+        return result;
     }
 };
