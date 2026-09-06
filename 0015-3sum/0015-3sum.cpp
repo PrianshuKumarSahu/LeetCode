290d71e7836676b1ee94efc59c151d6fcb9770f9
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-09-06 18:18:35 +0530

    Time: 48 ms (51.95%), Space: 31.1 MB (20.23%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& num) {
+
+        /*
         int n = num.size();
         vector<vector<int>> ans;
 
@@ -26,5 +28,56 @@ public:
             }
         }
         return ans;
+        */
+
+
+        //BETTER APPROACH
+        /*
+        int n = num.size();
+        set<vector<int>> st;
+        for (int i = 0; i<n; i++){
+            set<int> hashset;
+            for (int j = i+1; j<n; j++){
+                int third = -(num[i] + num[j]);
+                if (hashset.find(third) != hashset.end()){
+                    vector<int> temp = {num[i], num[j], third};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+                hashset.insert(num[j]);
+            }
+        }
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+        */
+
+        int n = num.size();
+        sort(num.begin(), num.end());
+        vector<vector<int>> ans;
+        for (int i=0 ; i<n ; i++){
+            if (i>0 && num[i] == num[i-1]) continue;
+
+            int j = i+1;
+            int k = n-1;
+            while (j < k){
+                int sum = num[i] + num[j] + num[k];
+                if (sum < 0){
+                    j++;
+                }
+                else if (sum > 0){
+                    k--;
+                }
+                else{
+                    vector <int> temp = {num[i], num[j], num[k]}; 
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && num[j] == num[j-1]) j++;
+                    while(j<k && num[k] == num[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+
     }
 };
