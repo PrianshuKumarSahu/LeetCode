1ade576af3ec13ef6dcfc6510b29b1a1127992c9
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-09-07 18:20:58 +0530

    Time: 0 ms (100%), Space: 17.6 MB (18.09%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
 
+    int startIndex(vector<int> &nums, int n, int target){
+        int low = 0;
+        int high = n-1;
+        int start = -1;
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target){
+                start = mid;
+                high = mid-1;
+            }else if (nums[mid] > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    int lastIndex(vector<int> &nums, int n, int target){
+        int low = 0;
+        int high = n-1;
+        int last = -1;
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target){
+                last = mid;
+                low = mid + 1;
+            }else if (nums[mid] > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return last;
+    }
+
+
+    /*
     int lowerBound(vector<int> &nums, int n, int target){
         int low = 0;
         int high = n-1;
@@ -34,11 +72,14 @@ public:
         }
         return ans;
     }
+    */
 
 
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
         vector <int> ans;
+        
+        /*
         int lb = lowerBound(nums, n, target);
         int ub = upperBound(nums, n, target);
 
@@ -48,6 +89,20 @@ public:
         }
 
         ans.insert(ans.end(),{lb, ub-1});
+        */
+
+        int start = startIndex(nums, n, target);
+        if (start == -1){
+            ans.push_back(-1);
+            ans.push_back(-1);
+            return ans;
+        }
+
+        int last = lastIndex(nums, n, target);
+
+        ans.push_back(start);
+        ans.push_back(last);
+
         return ans;
     }
 };
