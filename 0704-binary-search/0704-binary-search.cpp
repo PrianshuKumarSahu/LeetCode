f9136bbe787bcb1cf895cf26bffc05d75f683a8b
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-09-07 17:05:38 +0530

    Time: 0 ms (100%), Space: 31.5 MB (7.18%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-    /*int search(vector<int>& nums, int target) {
+    int search(vector<int>& nums, int target) {
         int n = nums.size();
-        int st = 0;
-        int end = n-1;
-        int ans = -1;
-        for (int i = 0; i<n; i++){
-            int mid = st + (end-st)/2; // (st+end)/2 , both are same 
-            if (target > nums[mid]){
-                st = mid+1;
-            }else if (target < nums[mid]){
-                end = mid-1;
-            }else{
-                ans = mid;
+        int left = 0;
+        int right = n-1;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if (nums[mid] == target){
+                return mid;
             }
-        }
-        return ans;
-    }*/
-
-    //helper function
-    int binSearch(vector<int>& arr, int tar, int st, int end){
-        if(st <= end){
-            int mid = st + (end-st)/2;
-
-            if(arr[mid] == tar) return mid;
-            else if(arr[mid] <= tar){
-                return binSearch(arr, tar, mid+1, end);
-            }else{
-                return binSearch(arr, tar, st, mid-1);
+            else if (nums[mid] > target){
+                right = mid - 1;
             }
-
+            else{
+                left = mid + 1;
+            }
         }
         return -1;
-    }
-
-    int search(vector<int>& arr, int tar){
-        return binSearch(arr, tar, 0, arr.size()-1);
     }
 };
