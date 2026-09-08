b997ca294676232adbb1a5bde153e6c32c2de7c4
Prianshu Kumar Sahu <prianshu1175@gmail.com>
2026-09-08 20:35:12 +0530

    Time: 1 ms (2.92%), Space: 15.3 MB (37.16%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -30,6 +30,7 @@ public:
         return -1;
         */
 
+        /*
         int n = nums.size();
         for (int i = 0; i<n; i++){
             if (nums[i] == target){
@@ -37,6 +38,35 @@ public:
             }
         }
 
+        return -1;
+        */
+
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        while (low <= high){
+            int mid = low + (high - low)/2;
+
+            if (nums[mid] == target){
+                return mid;
+            }
+            if (nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{
+                if (nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
         return -1;
     }
 };
